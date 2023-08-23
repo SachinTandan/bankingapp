eab3e1453d1cc60fbf35452757fb2538ae5ad8eb
Sachin Tandan <sachin8peace@gmail.com>
2023-08-23 01:38:14 -0500

    css for logo and latout for side bar

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import React from "react";
 
 //screen
 import Header from "./screens/header";
+import Sidebar from "./screens/sidebar";
 
 function App() {
     return (
-        <div className="App">
+        <div className="App flex">
+            <Sidebar/>
             <Header/>
 
         </div>
---
+++ src/screens/sidebar.js
@@ -0,0 +1,14 @@
+import logo from "../ui/logo-udemy.svg"
+
+function Sidebar() {
+    return (
+
+        <div className="sidebar fixed">
+            <a href='#' className="logo bl">
+                <img src={logo} className="bl"/>
+            </a>
+        </div>
+    )
+}
+
+export default Sidebar;
